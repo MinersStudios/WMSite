1bb044c26108e9d9201743df1607e9c369c84fa0
Poloskun_ <nikongricisin339@gmail.com>
2023-05-19 12:18:19 +0300

    js scroll

--- assets/index.js
+++ assets/index.js
@@ -1,1 +1,17 @@
+const anchorLinks = document.querySelectorAll('a[href*="#"]');
 
+anchorLinks.forEach(function(link) {
+    link.addEventListener("click", function(event) {
+        event.preventDefault();
+        const attribute = this.getAttribute('href');
+        const target = document.querySelector(attribute);
+        if (target) {
+            window.scrollTo({
+                top: target.offsetTop,
+                behavior: 'smooth'
+            });
+        } else {
+            console.error("Cannot resolve anchor " + attribute + " in file " + window.location.pathname);
+        }
+    });
+});
