19b1c3e4725c5e6552a24e94c65ef535be2d630d
Poloskun_ <nikongricisin339@gmail.com>
2023-05-19 18:50:15 +0300

    update

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,6 @@
 const anchorLinks = document.querySelectorAll('a[href*="#"]');
-
 anchorLinks.forEach(function(link) {
-    link.addEventListener("click", function(event) {
+    link.addEventListener('click', function(event) {
         event.preventDefault();
         const attribute = this.getAttribute('href');
         const target = document.querySelector(attribute);
@@ -15,3 +14,32 @@ anchorLinks.forEach(function(link) {
         }
     });
 });
+
+const ipButtons = document.querySelectorAll('button#ip');
+ipButtons.forEach(function(button) {
+    button.addEventListener('click', function() {
+        const ip = 'play.whomine.net';
+
+        navigator.clipboard.writeText(ip)
+            .then(function() {
+                showToast('Айпи скопирован!', 'green');
+            })
+            .catch(function() {
+                showToast('Айпи не удалось скопировать :(', 'red');
+                console.error('Failed to copy the IP, please use a better browser');
+            });
+    });
+});
+
+
+function showToast(text, color) {
+    const toastsContainer = document.querySelector('.toasts');
+    const toast = document.createElement('div');
+    toast.classList.add('toast', 'transitionIn', 'transitionOut', color);
+    toast.textContent = text;
+    toastsContainer.appendChild(toast);
+
+    setTimeout(function () {
+        toastsContainer.removeChild(toast);
+    }, 3500);
+}
