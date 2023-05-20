c7264a93ba21b8ef410e8f6276ff8b83986a1da8
Poloskun_ <nikongricisin339@gmail.com>
2023-05-20 23:36:28 +0300

    added team

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,5 @@
 const anchorLinks = document.querySelectorAll('a[href*="#"]');
-anchorLinks.forEach(function(link) {
+anchorLinks.forEach((link) => {
     link.addEventListener('click', function(event) {
         event.preventDefault();
         const attribute = this.getAttribute('href');
@@ -16,21 +16,20 @@ anchorLinks.forEach(function(link) {
 });
 
 const ipButtons = document.querySelectorAll('button#ip');
-ipButtons.forEach(function(button) {
+ipButtons.forEach((button) => {
     button.addEventListener('click', function() {
         const ip = 'play.whomine.net';
 
         navigator.clipboard.writeText(ip)
-            .then(function() {
+            .then(() => {
                 showToast('Айпи скопирован!', 'green');
             })
-            .catch(function() {
+            .catch((error) => {
                 showToast('Айпи не удалось скопировать :(', 'red');
-                console.error('Failed to copy the IP, please use a better browser');
+                console.error('Failed to copy the IP, please use a better browser: ', error);
             });
     });
 });
-
 
 function showToast(text, color) {
     const toastsContainer = document.querySelector('.toasts');
@@ -39,7 +38,102 @@ function showToast(text, color) {
     toast.textContent = text;
     toastsContainer.appendChild(toast);
 
-    setTimeout(function () {
+    setTimeout(() => {
         toastsContainer.removeChild(toast);
     }, 3500);
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    const arrowLeft = document.getElementById("team-arrow-left");
+    const arrowRight = document.getElementById("team-arrow-right");
+    const heads = Array.from(document.querySelectorAll(".head"));
+    const personalInfoContainers = Array.from(document.querySelectorAll(".personal-info > div"));
+    const bodyContainers = Array.from(document.querySelectorAll(".skin > div"));
+    const teamContainer = document.getElementById("team");
+
+    let currentIndex = 0;
+    let isUpdating = false;
+    let intervalId = null;
+
+    function updateSelectedIndex(index, previousIndex) {
+        if (isUpdating) return;
+        isUpdating = true;
+
+        heads[previousIndex].classList.remove("selected");
+
+        personalInfoContainers[previousIndex].classList.add("fadeText-leave-to", "fadeText-leave-active");
+        setTimeout(() => {
+            personalInfoContainers[previousIndex].classList.remove("selected", "fadeText-leave-to", "fadeText-leave-active");
+        }, 350);
+
+        bodyContainers[previousIndex].classList.add("fadeImg-leave-to", "fadeImg-leave-active");
+        setTimeout(() => {
+            bodyContainers[previousIndex].classList.remove("selected", "fadeImg-leave-to", "fadeImg-leave-active");
+        }, 350);
+
+        heads[index].classList.add("selected");
+
+        personalInfoContainers[index].classList.add("selected", "fadeText-enter-from", "fadeText-enter-active");
+        setTimeout(() => {
+            personalInfoContainers[index].classList.remove("fadeText-enter-from");
+        }, 10);
+        setTimeout(() => {
+            personalInfoContainers[index].classList.remove("fadeText-enter-active");
+            isUpdating = false;
+        }, 350);
+
+        bodyContainers[index].classList.add("selected", "fadeImg-enter-from", "fadeImg-enter-active");
+        setTimeout(() => {
+            bodyContainers[index].classList.remove("fadeImg-enter-from");
+        }, 10);
+        setTimeout(() => {
+            bodyContainers[index].classList.remove("fadeImg-enter-active");
+            isUpdating = false;
+        }, 350);
+    }
+
+    arrowRight.addEventListener("click", () => {
+        if (isUpdating) return;
+        const previousIndex = currentIndex;
+        currentIndex = (currentIndex + 1) % heads.length;
+        updateSelectedIndex(currentIndex, previousIndex);
+    });
+
+    arrowLeft.addEventListener("click", () => {
+        if (isUpdating) return;
+        const previousIndex = currentIndex;
+        currentIndex = (currentIndex - 1 + heads.length) % heads.length;
+        updateSelectedIndex(currentIndex, previousIndex);
+    });
+
+    heads.forEach((head) => {
+        head.addEventListener("click", function() {
+            if (isUpdating) return;
+            const headId = this.getAttribute("id");
+            const previousIndex = currentIndex;
+            const newIndex = heads.findIndex((head) => head.getAttribute("id") === headId);
+            if (newIndex !== currentIndex) {
+                currentIndex = newIndex;
+                updateSelectedIndex(currentIndex, previousIndex);
+            }
+        });
+    });
+
+    function startAutoScroll() {
+        intervalId = setInterval(() => {
+            const previousIndex = currentIndex;
+            currentIndex = (currentIndex + 1) % heads.length;
+            updateSelectedIndex(currentIndex, previousIndex);
+        }, 5000);
+    }
+
+    function stopAutoScroll() {
+        clearInterval(intervalId);
+    }
+
+    teamContainer.addEventListener("mouseenter", stopAutoScroll);
+    teamContainer.addEventListener("mouseleave", startAutoScroll);
+
+    updateSelectedIndex(currentIndex, 1);
+    startAutoScroll();
+});
