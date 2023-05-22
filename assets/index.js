1f81b7be7900a772d2c83dc37e042f28fd7911e2
Poloskun_ <nikongricisin339@gmail.com>
2023-05-22 21:27:58 +0300

    added new screenshots

--- assets/index.js
+++ assets/index.js
@@ -154,7 +154,24 @@ function getRandomScreenshot() {
         '7.png',
         '8.png',
         '9.png',
-        '10.png'
+        '10.png',
+        '11.png',
+        '12.png',
+        '13.png',
+        '14.png',
+        '15.png',
+        '17.png',
+        '18.png',
+        '19.png',
+        '20.png',
+        '22.png',
+        '23.png',
+        '24.png',
+        '25.png',
+        '26.png',
+        '27.png',
+        '28.png',
+        '30.png'
     ];
 
     const screenshotImg = document.getElementById('screenshot-img');
