8b709512358006b8d09304ab7bfb96f110fd3cce
Poloskun_ <nikongricisin339@gmail.com>
2023-09-19 22:09:30 +0300

    some booba

--- assets/index.js
+++ assets/index.js
@@ -1,143 +1,9 @@
-const anchorLinks = document.querySelectorAll('a[href*="#"]');
-anchorLinks.forEach((link) => {
-    link.addEventListener('click', function(event) {
-        event.preventDefault();
-        const attribute = this.getAttribute('href');
-        const target = document.querySelector(attribute);
-        if (target) {
-            window.scrollTo({
-                top: target.offsetTop,
-                behavior: 'smooth'
-            });
-        } else {
-            console.error("Cannot resolve anchor " + attribute + " in file " + window.location.pathname);
-        }
-    });
-});
-
-const ipButtons = document.querySelectorAll('button#ip');
-ipButtons.forEach((button) => {
-    button.addEventListener('click', function() {
-        const ip = 'play.whomine.net';
-
-        navigator.clipboard.writeText(ip)
-            .then(() => {
-                showToast('Айпи скопирован!', 'green');
-            })
-            .catch((error) => {
-                showToast('Айпи не удалось скопировать :(', 'red');
-                console.error('Failed to copy the IP, please use a better browser: ', error);
-            });
-    });
-});
-
-function showToast(text, color) {
-    const toastsContainer = document.querySelector('.toasts');
-    const toast = document.createElement('div');
-    toast.classList.add('toast', 'transitionIn', 'transitionOut', color);
-    toast.textContent = text;
-    toastsContainer.appendChild(toast);
-
-    setTimeout(() => {
-        toastsContainer.removeChild(toast);
-    }, 3500);
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-    const arrowLeft = document.getElementById("team-arrow-left");
-    const arrowRight = document.getElementById("team-arrow-right");
-    const heads = Array.from(document.querySelectorAll(".head"));
-    const personalInfoContainers = Array.from(document.querySelectorAll(".personal-info > div"));
-    const bodyContainers = Array.from(document.querySelectorAll(".skin > div"));
-    const teamContainer = document.getElementById("team");
-
-    let currentIndex = 0;
-    let isUpdating = false;
-    let intervalId = null;
-
-    function updateSelectedIndex(index, previousIndex) {
-        if (isUpdating) return;
-        isUpdating = true;
-
-        heads[previousIndex].classList.remove("selected");
-
-        personalInfoContainers[previousIndex].classList.add("fadeText-leave-to", "fadeText-leave-active");
-        setTimeout(() => {
-            personalInfoContainers[previousIndex].classList.remove("selected", "fadeText-leave-to", "fadeText-leave-active");
-        }, 350);
-
-        bodyContainers[previousIndex].classList.add("fadeImg-leave-to", "fadeImg-leave-active");
-        setTimeout(() => {
-            bodyContainers[previousIndex].classList.remove("selected", "fadeImg-leave-to", "fadeImg-leave-active");
-        }, 350);
-
-        heads[index].classList.add("selected");
-
-        personalInfoContainers[index].classList.add("selected", "fadeText-enter-from", "fadeText-enter-active");
-        setTimeout(() => {
-            personalInfoContainers[index].classList.remove("fadeText-enter-from");
-        }, 10);
-        setTimeout(() => {
-            personalInfoContainers[index].classList.remove("fadeText-enter-active");
-            isUpdating = false;
-        }, 350);
-
-        bodyContainers[index].classList.add("selected", "fadeImg-enter-from", "fadeImg-enter-active");
-        setTimeout(() => {
-            bodyContainers[index].classList.remove("fadeImg-enter-from");
-        }, 10);
-        setTimeout(() => {
-            bodyContainers[index].classList.remove("fadeImg-enter-active");
-            isUpdating = false;
-        }, 350);
-    }
-
-    arrowRight.addEventListener("click", () => {
-        if (isUpdating) return;
-        const previousIndex = currentIndex;
-        currentIndex = (currentIndex + 1) % heads.length;
-        updateSelectedIndex(currentIndex, previousIndex);
-    });
-
-    arrowLeft.addEventListener("click", () => {
-        if (isUpdating) return;
-        const previousIndex = currentIndex;
-        currentIndex = (currentIndex - 1 + heads.length) % heads.length;
-        updateSelectedIndex(currentIndex, previousIndex);
-    });
-
-    heads.forEach((head) => {
-        head.addEventListener("click", function() {
-            if (isUpdating) return;
-            const headId = this.getAttribute("id");
-            const previousIndex = currentIndex;
-            const newIndex = heads.findIndex((head) => head.getAttribute("id") === headId);
-            if (newIndex !== currentIndex) {
-                currentIndex = newIndex;
-                updateSelectedIndex(currentIndex, previousIndex);
-            }
-        });
-    });
-
-    function startAutoScroll() {
-        intervalId = setInterval(() => {
-            const previousIndex = currentIndex;
-            currentIndex = (currentIndex + 1) % heads.length;
-            updateSelectedIndex(currentIndex, previousIndex);
-        }, 5000);
-    }
-
-    function stopAutoScroll() {
-        clearInterval(intervalId);
-    }
-
-    teamContainer.addEventListener("mouseenter", stopAutoScroll);
-    teamContainer.addEventListener("mouseleave", startAutoScroll);
-
-    updateSelectedIndex(currentIndex, 1);
-    startAutoScroll();
-});
-
+const anchorLinks = document.querySelectorAll('a[href*="#"]')
+const anchorButtons = document.querySelectorAll('button[onclick*="#"]')
+const ipButtons = document.querySelectorAll('.ip-button')
+const hamburger = document.getElementById('hamburger')
+const navItems = document.getElementById('navItems')
+const navContent = document.getElementById('navContent')
 const screenshots = [
     '1.webp',
     '2.webp',
@@ -166,111 +32,301 @@ const screenshots = [
     '27.webp',
     '28.webp',
     '30.webp'
-];
+]
 
-function shuffle(array) {
-    for (let i = 0; i < array.length; ++i) {
-        const j = Math.floor(Math.random() * array.length);
-        [array[i], array[j]] = [array[j], array[i]];
+let isScreenshotLoading = false
+let screenshotsSeen = 0
+
+if (anchorLinks) {
+    for (const link of anchorLinks) {
+        link.addEventListener('click', function(event) {
+            event.preventDefault()
+
+            const attribute = this.getAttribute('href')
+            const target = document.querySelector(attribute)
+
+            if (target) {
+                scrollTo(target)
+            } else {
+                console.error('Cannot resolve anchor ' + attribute + ' in file ' + window.location.pathname)
+            }
+        })
     }
 }
 
-let isScreenshotLoading = false;
-let screenshotsSeen = 0;
+if (anchorButtons) {
+    for (const button of anchorButtons) {
+        button.addEventListener('click', function(event) {
+            event.preventDefault()
+
+            const attribute = this.getAttribute('onclick').split('\'')[1]
+            const target = document.querySelector(attribute)
+
+            if (target) {
+                scrollTo(target)
+            } else {
+                console.error('Cannot resolve anchor ' + attribute + ' in file ' + window.location.pathname)
+            }
+        })
+    }
+}
+
+if (ipButtons) {
+    for (const button of ipButtons) {
+        button.addEventListener('click', function() {
+            const ip = 'play.whomine.net'
+
+            navigator.clipboard
+            .writeText(ip)
+            .then(() => {
+                showToast('Айпи скопирован!', 'green')
+            })
+            .catch((error) => {
+                showToast('Айпи не удалось скопировать :(', 'red')
+                console.error('Failed to copy the IP, please use a better browser: ', error)
+            })
+        })
+    }
+}
+
+hamburger.addEventListener('click', function() {
+    navItems.classList.toggle('open')
+    navContent.classList.toggle('open')
+
+    setTimeout(function() {
+        if (!navItems.classList.contains('open')) {
+            navItems.style.display = ''
+        } else {
+            navItems.style.display = 'flex'
+        }
+    }, 350)
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    const heads = document.querySelectorAll('.head')
+    const personalInfoContainers = document.querySelectorAll('.personal-info > div')
+    const bodyContainers = document.querySelectorAll('.skin > div')
+    const teamContainer = document.getElementById('team')
+
+    let currentIndex = 0
+    let isUpdating = false
+    let intervalId = null
+
+    document.getElementById('team-arrow-right').addEventListener('click', () => {
+        if (isUpdating) return
+
+        const previousIndex = currentIndex
+        currentIndex = (currentIndex + 1) % heads.length
+
+        updateSelectedIndex(currentIndex, previousIndex)
+    })
+
+    document.getElementById('team-arrow-left').addEventListener('click', () => {
+        if (isUpdating) return
+
+        const previousIndex = currentIndex
+        currentIndex = (currentIndex - 1 + heads.length) % heads.length
+
+        updateSelectedIndex(currentIndex, previousIndex)
+    })
+
+    heads.forEach((head) => {
+        head.addEventListener('click', function() {
+            if (isUpdating) return
+
+            const previousIndex = currentIndex
+            const newIndex = heads.findIndex(
+                head => head.getAttribute('id') === this.getAttribute('id')
+            )
+
+            if (newIndex !== currentIndex) {
+                updateSelectedIndex(currentIndex = newIndex, previousIndex)
+            }
+        })
+    })
+
+    teamContainer.addEventListener('mouseenter', stopAutoScroll)
+    teamContainer.addEventListener('mouseleave', startAutoScroll)
+
+    updateSelectedIndex(currentIndex, 1)
+    startAutoScroll()
+
+    function updateSelectedIndex(index, previousIndex) {
+        if (isUpdating) return
+        isUpdating = true
+
+        heads[previousIndex].classList.remove('selected')
+
+        personalInfoContainers[previousIndex].classList.add('fadeText-leave-to', 'fadeText-leave-active')
+        setTimeout(() => {
+            personalInfoContainers[previousIndex].classList.remove('selected', 'fadeText-leave-to', 'fadeText-leave-active')
+        }, 350)
+
+        bodyContainers[previousIndex].classList.add('fadeImg-leave-to', 'fadeImg-leave-active')
+        setTimeout(() => {
+            bodyContainers[previousIndex].classList.remove('selected', 'fadeImg-leave-to', 'fadeImg-leave-active')
+        }, 350)
+
+        heads[index].classList.add('selected')
+
+        personalInfoContainers[index].classList.add('selected', 'fadeText-enter-from', 'fadeText-enter-active')
+        setTimeout(() => {
+            personalInfoContainers[index].classList.remove('fadeText-enter-from')
+        }, 10)
+        setTimeout(() => {
+            personalInfoContainers[index].classList.remove('fadeText-enter-active')
+            isUpdating = false
+        }, 350)
+
+        bodyContainers[index].classList.add('selected', 'fadeImg-enter-from', 'fadeImg-enter-active')
+        setTimeout(() => {
+            bodyContainers[index].classList.remove('fadeImg-enter-from')
+        }, 10)
+        setTimeout(() => {
+            bodyContainers[index].classList.remove('fadeImg-enter-active')
+            isUpdating = false
+        }, 350)
+    }
+
+    function startAutoScroll() {
+        intervalId = setInterval(() => {
+            const previousIndex = currentIndex
+            currentIndex = (currentIndex + 1) % heads.length
+            updateSelectedIndex(currentIndex, previousIndex)
+        }, 5000)
+    }
+
+    function stopAutoScroll() {
+        clearInterval(intervalId)
+    }
+})
+
+document.getElementById('random-button').addEventListener('click', getRandomScreenshot)
+document.addEventListener('scroll', handleScroll)
+
+getRandomScreenshot()
+handleScroll()
+
+function scrollTo(target) {
+    if (target.offsetHeight < window.innerHeight) {
+        window.scrollTo({
+            top: target.offsetTop - (window.innerHeight - target.offsetHeight) / 2,
+            behavior: 'smooth'
+        })
+    } else {
+        window.scrollTo({
+            top: target.offsetTop,
+            behavior: 'smooth'
+        })
+    }
+}
+
+function showToast(text, color) {
+    const toastsContainer = document.querySelector('.toasts')
+    const toast = document.createElement('div')
+    toast.classList.add('toast', 'transitionIn', 'transitionOut', color)
+    toast.textContent = text
+    toastsContainer.appendChild(toast)
+
+    setTimeout(() => {
+        toastsContainer.removeChild(toast)
+    }, 3500)
+}
 
 function getRandomScreenshot() {
-    if (isScreenshotLoading) return;
-
+    if (isScreenshotLoading) return
     if (screenshotsSeen === screenshots.length) {
-        const lastBitch = screenshots[screenshotsSeen];
-        shuffle(screenshots);
-        screenshotsSeen = 0;
-        const thatOne = screenshots.indexOf(lastBitch);
-        if (!thatOne) [screenshots[0], screenshots[thatOne]] = [screenshots[thatOne], screenshots[0]];
+        const that = screenshots.indexOf(screenshots[screenshotsSeen])
+
+        shuffle(screenshots)
+
+        screenshotsSeen = 0
+
+        if (!that) {
+            [screenshots[0], screenshots[that]] = [screenshots[that], screenshots[0]]
+        }
     }
 
-    const screenshot = screenshots[screenshotsSeen];
-    const screenshotImg = document.getElementById('screenshot-img');
-    screenshotImg.style.opacity = "0";
+    const screenshot = screenshots[screenshotsSeen]
+    const screenshotImg = document.getElementById('screenshot-img')
+    screenshotImg.style.opacity = '0'
+
     setTimeout(() => {
-        screenshotImg.style.backgroundImage = `url(./assets/img/screenshots/${screenshot})`;
-    }, 350);
-    isScreenshotLoading = true;
+        screenshotImg.style.backgroundImage = `url(./assets/img/screenshots/${screenshot})`
+    }, 350)
 
-    const image = new Image();
-    image.src = './assets/img/screenshots/' + screenshot;
+    isScreenshotLoading = true
+
+    const image = new Image()
+    image.src = './assets/img/screenshots/' + screenshot
     image.onload = function() {
-        const brightness = calculateBrightness(image);
-        updateButtonColor(brightness);
+        updateButtonColor(calculateBrightness(image))
         setTimeout(() => {
-            screenshotImg.classList.add('loaded');
-            screenshotImg.style.opacity = "1";
-        }, 350);
+            screenshotImg.classList.add('loaded')
+            screenshotImg.style.opacity = '1'
+        }, 350)
         setTimeout(() => {
-            isScreenshotLoading = false;
-        }, 500);
-    };
-    screenshotsSeen += 1;
-}
-
-function calculateBrightness(image) {
-    const canvas = document.createElement('canvas');
-    canvas.width = image.width;
-    canvas.height = image.height;
-    const context = canvas.getContext('2d');
-    context.drawImage(image, 0, 0);
-    return getAverageBrightness(context.getImageData(0, 0, image.width, image.height));
-}
-
-function getAverageBrightness(imageData) {
-    let brightnessSum = 0;
-    const data = imageData.data;
-    const pixelCount = data.length / 4;
-
-    for (let i = 0; i < pixelCount; i++) {
-        const r = data[i * 4];
-        const g = data[i * 4 + 1];
-        const b = data[i * 4 + 2];
-        const brightnessValue = (r + g + b) / 3;
-        brightnessSum += brightnessValue;
+            isScreenshotLoading = false
+        }, 500)
     }
 
-    return brightnessSum / pixelCount;
+    screenshotsSeen += 1
 }
 
 function updateButtonColor(brightness) {
-    const button = document.getElementById('random-button');
+    const button = document.getElementById('random-button')
+
     if (brightness > 128) {
-        button.classList.add('dark');
+        button.classList.add('dark')
     } else {
-        button.classList.remove('dark');
+        button.classList.remove('dark')
     }
 }
 
-document.getElementById('random-button').addEventListener('click', getRandomScreenshot);
+function calculateBrightness(image) {
+    const canvas = document.createElement('canvas')
+    canvas.width = image.width
+    canvas.height = image.height
 
-getRandomScreenshot();
+    const context = canvas.getContext('2d')
+    context.drawImage(image, 0, 0)
+
+    return getAverageBrightness(context.getImageData(0, 0, image.width, image.height).data)
+}
+
+function getAverageBrightness(data) {
+    let brightnessSum = 0
+    const pixelCount = data.length / 4
+
+    for (let i = 0; i < pixelCount; i++) {
+        brightnessSum += (data[i] + data[i + 1] + data[i + 2]) / 3
+    }
+
+    return brightnessSum / pixelCount
+}
 
 function isElementInViewport(element, threshold = 125) {
-    const rect = element.getBoundingClientRect();
-    const windowHeight = window.innerHeight || document.documentElement.clientHeight;
-    const windowWidth = window.innerWidth || document.documentElement.clientWidth;
-    const topVisible = rect.top + threshold < windowHeight;
-    const bottomVisible = rect.bottom - threshold > 0;
-    const leftVisible = rect.left + threshold < windowWidth;
-    const rightVisible = rect.right - threshold > 0;
-    return topVisible && bottomVisible && leftVisible && rightVisible;
+    const rect = element.getBoundingClientRect()
+    const windowHeight = window.innerHeight || document.documentElement.clientHeight
+    const windowWidth = window.innerWidth || document.documentElement.clientWidth
+    const topVisible = rect.top + threshold < windowHeight
+    const bottomVisible = rect.bottom - threshold > 0
+    const leftVisible = rect.left + threshold < windowWidth
+    const rightVisible = rect.right - threshold > 0
+    return topVisible && bottomVisible && leftVisible && rightVisible
 }
 
 function handleScroll() {
-    const articles = document.querySelectorAll('article');
-    for (const article of articles) {
+    for (const article of document.querySelectorAll('article')) {
         if (isElementInViewport(article)) {
-            article.classList.add('animate');
+            article.classList.add('animate')
         }
     }
 }
 
-window.addEventListener('scroll', handleScroll);
-
-handleScroll();
+function shuffle(array) {
+    for (let i = 0; i < array.length; ++i) {
+        const j = Math.floor(Math.random() * array.length);
+        [array[i], array[j]] = [array[j], array[i]]
+    }
+}
